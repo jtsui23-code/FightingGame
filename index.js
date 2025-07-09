0aceb052b6c144f3a34c1b213521dc6872f95ca6
Jack Tsui <jacktsui23@gmail.com>
2025-07-09 17:19:39 -0500

    Creating basis project setup for the fighting game.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Grabs the canvas item in the index.html page
+const canvas = document.querySelector('canvas')
+
+// c - context shorthand because using it quite often. Responsible 
+// for drawing sprites for the game.
+const c = canvas.getContext('2d')
+
+
